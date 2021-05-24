587b8b3753215acf791cff0bf3b71ac41e51757e
Casey Bailey <soniczentropy@gmail.com>
2021-05-24 10:56:46 -0500

    Updated

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git stash
+git checkout master
+git reset --hard dev
+
+zola build
+
+mv public/* .
+rm -rf config.toml content sass static themes templaes
+echo "zentropy.live" >> CNAME
+git add -f .
+git commit -m "Build"
+
+git push -f origin master
+
+# Cleanup
+git checkout dev
+git clean -f -d
+git stash pop
---
+++ content/pages/about.md
@@ -0,0 +1,8 @@
++++
+title = "about us"
+path = "about"
++++
+
+‣ [github](https://github.com/soniczentropy)
+
+‣ [projects](https://zentropy.live/projects)
